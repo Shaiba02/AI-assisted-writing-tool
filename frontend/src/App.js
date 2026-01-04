0a6c38ed69891e6ab0a250d87a7be7f1e60b5236
Shaiba02 <shaibamaji02@gmail.com>
2026-01-04 22:46:28 +0530

    folders added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      AI assisted writing tool
+    </div>
+  );
+}
+
+export default App;
